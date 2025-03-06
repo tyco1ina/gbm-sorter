aef0dfbc6d0a6d4f694e658638efb9d8848dd899
Tiberius Colina <tiberiuscolina@lawn-128-61-124-98.lawn.gatech.edu>
2025-03-06 09:55:01 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,291 @@
+import { useState } from 'react'
+import trolleyImage from './assets/trolley.jpg' // Import the image
+import './App.css'
+
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { handleData, getGroupings } from './utils.js'
+
+function App() {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [groupings, setGroupings] = useState({
+    'duwende': [],
+    'aswang': [],
+    'mariaMakiling': [],
+    'idiyanale': [],
+    'mapulon': []
+  });
+
+  const handleSortGroups = async () => {
+    const response = await getGroupings();
+    if (response) {
+      setGroupings(response);
+    } else {
+      alert('Error fetching groupings');
+    }
+  }
+
+  const handleSubmit = async () => {
+
+    setSubmitting(true);
+
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const gtEmail = document.getElementById('gtEmail').value;
+    const year = document.querySelector('input[name="year"]:checked')?.value;
+    const mailingList = document.querySelector('input[name="mailingList"]:checked')?.value;
+    const superpower = document.querySelector('input[name="superpower"]:checked')?.value;
+    const freeTime = document.querySelector('input[name="freeTime"]:checked')?.value;
+    const hpHouse = document.querySelector('input[name="hpHouse"]:checked')?.value;
+    const lifetime = document.querySelector('input[name="lifetime"]:checked')?.value;
+    const trolley = document.querySelector('input[name="trolley"]:checked')?.value;
+
+    const result = await handleData({
+      firstName,
+      lastName,
+      gtEmail,
+      year,
+      mailingList,
+      superpower,
+      freeTime,
+      hpHouse,
+      lifetime,
+      trolley
+    });
+
+    alert(result);
+    if (result === 'Information submitted successfully!') {
+      setSubmitted(true);
+    }
+    setSubmitting(false);
+    return
+
+  };
+
+  const handleAdminSettings = () => {
+    const password = prompt('Enter admin password:');
+    if (password === 'halohalo') {
+      setIsAdmin(true);
+    } else {
+      alert('Access denied.');
+    }
+  };
+
+  const handleBackToQuiz = () => {
+    setIsAdmin(false);
+  };
+
+  if (isAdmin) {
+    const groups = Object.values(groupings);
+
+    return (
+      <div className="admin-container">
+        <button onClick={() => handleSortGroups()}>Sort Groups</button>
+        <button onClick={() => alert('Randomly Sort Groups')}>Randomly Sort Groups</button>
+        <button onClick={() => alert('Clear Table')}>Clear Table</button>
+        <button onClick={handleBackToQuiz}>Back to Quiz</button>
+        <table className="admin-table">
+          <thead>
+            <tr>
+              <th>Group 1</th>
+              <th>Group 2</th>
+              <th>Group 3</th>
+              <th>Group 4</th>
+              <th>Group 5</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {groups.map((group, colIndex) => (
+                <td key={colIndex}>
+                  {group.map((name, rowIndex) => (
+                    <div key={rowIndex}>{name}</div>
+                  ))}
+                </td>
+              ))}
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  if (submitted) {
+    return (
+      <div className="thank-you-container">
+        <h3>Thank you for submitting the quiz!</h3>
+        <p>Please enjoy the food and the first activity will start soon.</p>
+      </div>
+
+    )
+  }
+
+  return (
+    <>
+      <h3>March GBM Sign-In and Quiz</h3>
+      <div className="question">
+        <label htmlFor="firstName">First Name</label>
+        <input type="text" id="firstName" name="firstName" className="input-box" />
+      </div>
+      <div className="question">
+        <label htmlFor="lastName">Last Name</label>
+        <input type="text" id="lastName" name="lastName" className="input-box" />
+      </div>
+      <div className="question">
+        <p>Year</p>
+        <div className="option">
+          <input type="radio" id="1stYear" name="year" value="1st Year" />
+          <label htmlFor="1stYear">1st Year</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="2ndYear" name="year" value="2nd Year" />
+          <label htmlFor="2ndYear">2nd Year</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="3rdYear" name="year" value="3rd Year" />
+          <label htmlFor="3rdYear">3rd Year</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="4thYear" name="year" value="4th Year" />
+          <label htmlFor="4thYear">4th Year</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="graduateStudent" name="year" value="Graduate Student" />
+          <label htmlFor="graduateStudent">Graduate Student</label>
+        </div>
+      </div>
+      <div className="question">
+        <label htmlFor="gtEmail">GT Email</label>
+        <input type="email" id="gtEmail" name="gtEmail" className="input-box" />
+      </div>
+      <div className="question">
+        <p>Would you like to join our mailing list to receive updates about our upcoming events?</p>
+        <div className="option">
+          <input type="radio" id="mailingListYes" name="mailingList" value="Yes" />
+          <label htmlFor="mailingListYes">Yes</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="mailingListNo" name="mailingList" value="No" />
+          <label htmlFor="mailingListNo">No</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="mailingListAlready" name="mailingList" value="Already on it!" />
+          <label htmlFor="mailingListAlready">Already on it!</label>
+        </div>
+      </div>
+      <div className="question">
+        <p>Choose a superpower:</p>
+        <div className="option">
+          <input type="radio" id="invisibility" name="superpower" value="Invisibility" />
+          <label htmlFor="invisibility">Invisibility</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="shapeshift" name="superpower" value="Shapeshift into anyone or anything" />
+          <label htmlFor="shapeshift">Shapeshifting</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="goodLooking" name="superpower" value="Supernaturally good looking" />
+          <label htmlFor="goodLooking">Supernaturally good looking</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="ultraIntellect" name="superpower" value="Ultra-intellect" />
+          <label htmlFor="ultraIntellect">Ultra-intellect</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="controlElements" name="superpower" value="Control the four elements: water, air earth and fire (like the Avatar)" />
+          <label htmlFor="controlElements">Control fire, air, earth and water</label>
+        </div>
+      </div>
+      <div className="question">
+        <p>You have an hour of free time. What do you do with it?</p>
+        <div className="option">
+          <input type="radio" id="tiktok" name="freeTime" value="TikTok or Instagram Reels" />
+          <label htmlFor="tiktok">TikTok or Instagram Reels</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="amongUs" name="freeTime" value="Among Us" />
+          <label htmlFor="amongUs">Among Us</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="hinge" name="freeTime" value="Hinge" />
+          <label htmlFor="hinge">Hinge</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="readBook" name="freeTime" value="Read a book" />
+          <label htmlFor="readBook">Read a book</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="goOutside" name="freeTime" value="Go outside" />
+          <label htmlFor="goOutside">Go outside</label>
+        </div>
+      </div>
+      <div className="question">
+        <p>Choose a Harry Potter house:</p>
+        <div className="option">
+          <input type="radio" id="slytherin" name="hpHouse" value="Slytherin" />
+          <label htmlFor="slytherin">Slytherin</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="voldemort" name="hpHouse" value="I’d rather work for Voldemort" />
+          <label htmlFor="voldemort">I’d rather work for Voldemort</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="hufflepuff" name="hpHouse" value="Hufflepuff" />
+          <label htmlFor="hufflepuff">Hufflepuff</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="ravenclaw" name="hpHouse" value="Ravenclaw" />
+          <label htmlFor="ravenclaw">Ravenclaw</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="gryffindor" name="hpHouse" value="Gryffindor" />
+          <label htmlFor="gryffindor">Gryffindor</label>
+        </div>
+      </div>
+      <div className="question">
+        <p>Choose one thing to happen in your lifetime:</p>
+        <div className="option">
+          <input type="radio" id="justice" name="lifetime" value="Bring justice to the world" />
+          <label htmlFor="justice">Bring justice to the world</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="powerful" name="lifetime" value="Become the most powerful person on the planet" />
+          <label htmlFor="powerful">Take over the world</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="trueLove" name="lifetime" value="Find true love" />
+          <label htmlFor="trueLove">Find true love</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="sevenFigures" name="lifetime" value="Make a seven-figure salary" />
+          <label htmlFor="sevenFigures">Make a seven-figure salary</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="oneWithNature" name="lifetime" value="Become one with nature" />
+          <label htmlFor="oneWithNature">Become one with nature</label>
+        </div>
+      </div>
+      <div className="question">
+        <img src={trolleyImage} alt="Trolley on fire" style={{ maxWidth: '300px' }} />
+        <p>A trolley on fire is heading towards a forest the size of Manhattan which is 700 miles from a large city. You can pull the lever to divert it to a different track, but it will run over and burn thirty million dollars that would have been yours. If you don’t pull the lever, the trolley will crash into the forest and burn it to the ground. There are no people in this forest, and you would not be blamed for the forest burning down. What do you do?</p>
+        <div className="option">
+          <input type="radio" id="pullLever" name="trolley" value="Pull the lever" />
+          <label htmlFor="pullLever">Pull the lever</label>
+        </div>
+        <div className="option">
+          <input type="radio" id="doNothing" name="trolley" value="Do nothing" />
+          <label htmlFor="doNothing">Do nothing</label>
+        </div>
+      </div>
+      <div className="button-container">
+        <button onClick={handleSubmit} disabled={submitting}>{submitting ? "Submitting..." : "Submit" }</button>
+        <button onClick={handleAdminSettings}>Admin Settings</button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/utils.js
@@ -0,0 +1,183 @@
+/**
+ * @description
+ * This function determines the groupings for the GBM
+ * @returns {Object} - The groupings for the GBM
+ */
+export const getGroupings = async () => {
+    let groupings = {
+      'duwende': [],
+      'aswang': [],
+      'mariaMakiling': [],
+      'idiyanale': [],
+      'mapulon': []
+    }
+
+    // Get the attendee information from the table
+    const response = await fetch('https://l11sh25g55.execute-api.us-east-1.amazonaws.com/get-attendee-info', {
+      method: 'GET',
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      
+      // Set the maximum group size
+      const maxGroupSize = Math.ceil(data.length / 5);
+
+      for (let i = 0; i < data.length; i++) {
+        const fullName = data[i]['firstName'] + ' ' + data[i]['lastName'];
+        const ranking = data[i]['ranking'];
+        for (let j = 0; j < 5; j++) {
+          const figure = data[i]['ranking'][j];
+          if (groupings[figure].length < maxGroupSize) {
+            groupings[figure].push(fullName);
+            break;
+          }
+        }
+      }
+
+      console.log(groupings)
+      return groupings; // Assuming the response contains the list of attendees
+  } else {
+      console.error('Error fetching attendee information:', response.statusText);
+      return null;
+  }
+}
+    
+
+/**
+ * @description
+ * This function calculates the figure alignment ranking
+ * @param {Object} data - The data submitted
+ */
+
+export const getAlignmentRanking = (data) => {
+    const { superpower, freeTime, hpHouse, lifetime, trolley } = data;
+
+    let rankings = {
+        'duwende': 0,
+        'aswang': 0,
+        'mariaMakiling': 0,
+        'idiyanale': 0,
+        'mapulon': 0
+    }
+
+    // Update the rankings based on the answers
+    const updateRanking = (answer, rankings) => {
+        switch (answer) {
+            case 'Invisibility':
+            case 'TikTok or Instagram Reels':
+            case 'Slytherin':
+            case 'Bring justice to the world':
+                rankings.duwende += 1;
+                break;
+            case 'Shapeshift into anyone or anything':
+            case 'Among Us':
+            case 'I’d rather work for Voldemort':
+            case 'Become the most powerful person on the planet':
+                rankings.aswang += 1;
+                break;
+            case 'Supernaturally good looking':
+            case 'Hinge':
+            case 'Hufflepuff':
+            case 'Find true love':
+                rankings.mariaMakiling += 1;
+                break;
+            case 'Ultra-intellect':
+            case 'Read a book':
+            case 'Ravenclaw':
+            case 'Make a seven-figure salary':
+                rankings.idiyanale += 1;
+                break;
+            case 'Control the four elements: water, air earth and fire (like the Avatar)':
+            case 'Go outside':
+            case 'Gryffindor':
+            case 'Become one with nature':
+                rankings.mapulon += 1;
+                break;
+            default:
+                break;
+        }
+    };
+
+    updateRanking(superpower, rankings);
+    updateRanking(freeTime, rankings);
+    updateRanking(hpHouse, rankings);
+    updateRanking(lifetime, rankings);
+    updateRanking(trolley, rankings);
+
+    // Update scores for trolley problem
+    if (trolley === 'Pull the lever') {
+        rankings.duwende += 2;
+        rankings.aswang += 2;
+    } else if (trolley === 'Do nothing') {
+        rankings.idiyanale += 2;
+        rankings.mariaMakiling += 2;
+        rankings.mapulon += 2;
+    }
+
+    const sortedRankings = Object.fromEntries(
+        Object.entries(rankings).sort(([, v1], [, v2]) => v1 - v2)
+    );
+
+    
+    // Get the top 5 rankings in the form of an array with only the figures
+    let rankingArray = Object.entries(sortedRankings).map(([key]) => key);
+    rankingArray = rankingArray.reverse();
+
+    return rankingArray;
+
+}
+
+/**
+ * @description
+ * This function handles the data from submitting the form
+ *  
+ * @param {Object} data - The data submitted
+ */
+export const handleData = async (data) => {
+
+    const { firstName, lastName, gtEmail, year, mailingList, superpower, freeTime, hpHouse, lifetime, trolley } = data;
+
+    // Check if the data is valid
+    if (!firstName) { return "Please enter your first name."; }
+    if (!lastName) { return 'Please enter your last name.'; }
+    if (!gtEmail) { return 'Please enter your GT email.';  }
+    if (!year) { return 'Please select your year.'; }
+    if (!mailingList) { return 'Please select if you would like to join our mailing list.'; }
+    if (!superpower) { return 'Please select a superpower.'; }
+    if (!freeTime) { return 'Please select what you would do with an hour of free time.'; }
+    if (!hpHouse) { return 'Please select a Harry Potter house.'; }
+    if (!lifetime) { return 'Please select one thing to happen in your lifetime.'; }
+    if (!trolley) { return 'Please select what you would do with the trolley.'; }
+
+    let result = getAlignmentRanking(data);
+    let email = gtEmail;
+
+    try {
+        const response = await fetch('https://l11sh25g55.execute-api.us-east-1.amazonaws.com/submit-form', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            firstName,
+            lastName,
+            email,
+            year,
+            mailingList,
+            result
+          })
+        });
+
+        console.log(response)
+    
+        if (response.ok) {
+          return 'Information submitted successfully!'
+        } else {
+          return 'Error submitting information.';
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        return 'Error submitting information.';
+      }
+}
